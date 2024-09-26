929962e755879a84ba37c327506eafac9fdb8d62
ekaterina epishina laptop <katerinka_epishina@mail.ru>
2024-09-26 15:45:25 +0400

    формы кроме списков

--- src/app/components/advertisement/advertisement.component.ts
+++ src/app/components/advertisement/advertisement.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-advertisement',
   standalone: true,
   imports: [],
   templateUrl: './advertisement.component.html',
-  styleUrl: './advertisement.component.css'
+  styleUrl: './advertisement.component.css',
 })
 export class AdvertisementComponent {
+  constructor(private router: Router) {
+  }
 
+  go_to_discussion_list() {
+    this.router.navigate(['/api/discussions']);
+  }
+
+  go_to_create_discussion() {
+    this.router.navigate(['/api/create_discussion']);
+  }
 }
