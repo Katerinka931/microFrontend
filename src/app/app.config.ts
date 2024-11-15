ea766d71b9e51b4fea04c03d733d6bd5e9fd116c
ekaterina epishina laptop <katerinka_epishina@mail.ru>
2024-11-15 18:40:50 +0400

    добавление интерсептора

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,15 +3,16 @@ import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
 import {provideClientHydration} from '@angular/platform-browser';
-import {provideHttpClient, withFetch} from "@angular/common/http";
+import {provideHttpClient, withFetch, withInterceptors} from "@angular/common/http";
 import {CommonModule, NgForOf} from "@angular/common";
+import {authInterceptor} from "./helper/interceptor";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
     provideClientHydration(),
-    provideHttpClient(withFetch()),
+    provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
     CommonModule,
     NgForOf,
   ]
---
+++ src/app/helper/interceptor.ts
@@ -0,0 +1,14 @@
+import {HttpEvent, HttpHandlerFn, HttpInterceptorFn, HttpRequest} from '@angular/common/http';
+import {catchError, Observable, throwError} from 'rxjs';
+
+
+export const authInterceptor: HttpInterceptorFn = (request: HttpRequest<any>, next: HttpHandlerFn): Observable<HttpEvent<any>> => {
+  var token = localStorage.getItem('token');
+  if (token) {
+    request = request.clone({headers: request.headers.set('Authorization', 'Bearer ' + token)});
+  }
+
+  return next(request).pipe(catchError(error => {
+    return throwError(error);
+  }));
+};
