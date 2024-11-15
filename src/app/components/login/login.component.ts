4130f1e9417b00cd88edad3c9095bf42dfef2269
ekaterina epishina laptop <katerinka_epishina@mail.ru>
2024-11-15 17:18:26 +0400

    настройка аутентификации

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,58 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {LoginService} from "../../services/login_service/login.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  registerForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private loginService: LoginService, private router: Router) {
+    this.loginForm = this.fb.group({
+      login_username: ['', Validators.required],
+      login_password: ['', Validators.required]
+    })
+    this.registerForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+      re_password: ['', Validators.required],
+    })
+  }
+
+  login() {
+    if (this.loginForm.valid) {
+      this.loginService.login(this.loginForm.value).subscribe({
+        next: (data) => {
+          localStorage.setItem('token', data['token'])
+          localStorage.setItem('role', data['role'])
+          this.router.navigate(["/api/advertisement/"]);
+        }, error: (e) => {
+          console.log(e);
+        }
+      })
+    }
+  }
+
+  register() {
+    if (this.registerForm.valid) {
+      if (this.registerForm.value['password'] == this.registerForm.value['re_password']) {
+        this.loginService.register(this.registerForm.value).subscribe({
+          next: (message) => {
+            console.log(message)
+          }, error: (e) => {
+            console.log(e);
+          }
+        })
+      } else {
+        alert('Пароли не совпадают!')
+      }
+    }
+  }
 }
--- src/app/services/login_service/login.service.ts
+++ src/app/services/login_service/login.service.ts
@@ -1,9 +1,43 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {environment} from "../../../environments/environment.development";
+import {catchError, Observable, tap, throwError} from "rxjs";
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  private baseUrl = environment.apiUrl + '/auth'
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      console.error('Ошибка на стороне клиента:', error.error);
+    } else {
+      console.error(
+        `Ошибка сервера с кодом ${error.status}, ошибка: `, error.error);
+    }
+    return throwError(() => new Error('Что-то пошло не так, попробуйте еще раз позже.'));
+  }
+
+  login(data: any): Observable<any> {
+    let d = {'username': data['login_username'], 'password': data['login_password']}
+    return this.http.post(`${this.baseUrl}/login`, d).pipe(
+      // catchError(this.handleError)
+    );
+  }
+
+  register(data: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}/register`, data).pipe(
+      // catchError(this.handleError)
+    );
+  }
+
+  getAuthToken(): string | null {
+    console.log('authentication')
+    console.log(localStorage.getItem('Authentication'))
+    return localStorage.getItem('Authentication');
+  }
 }
