db50ba5e26809f7bc9a7ebf591e56742e0111e38
ekaterina epishina laptop <katerinka_epishina@mail.ru>
2024-11-15 18:41:32 +0400

    редактирование контроллера логина

--- src/app/services/login_service/login.service.ts
+++ src/app/services/login_service/login.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {environment} from "../../../environments/environment.development";
-import {catchError, Observable, tap, throwError} from "rxjs";
+import {catchError, Observable, throwError} from "rxjs";
 import {HttpClient, HttpErrorResponse} from "@angular/common/http";
 
 @Injectable({
@@ -25,19 +25,13 @@ export class LoginService {
   login(data: any): Observable<any> {
     let d = {'username': data['login_username'], 'password': data['login_password']}
     return this.http.post(`${this.baseUrl}/login`, d).pipe(
-      // catchError(this.handleError)
+      catchError(this.handleError)
     );
   }
 
   register(data: any): Observable<any> {
     return this.http.post(`${this.baseUrl}/register`, data).pipe(
-      // catchError(this.handleError)
+      catchError(this.handleError)
     );
-  }
-
-  getAuthToken(): string | null {
-    console.log('authentication')
-    console.log(localStorage.getItem('Authentication'))
-    return localStorage.getItem('Authentication');
   }
 }
